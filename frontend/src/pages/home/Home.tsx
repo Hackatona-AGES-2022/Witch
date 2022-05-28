175b68541868dc52b2633ea73df9b9ac43d305a4
Aléxia Dorneles <alexiadorneles02@gmail.com>
2022-05-28 05:39:28 -0300

    feat(feed): fetch from backend

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,20 +1,30 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/header/Header";
 import { Navbar } from "../../components/navbar/Navbar";
 import { Post } from "../../components/post/Post";
+import { useApi } from "../../hooks/useApi";
 import styles from "./Home.module.css";
 
 export function Home() {
+  const [feed, setFeed] = useState([]);
+
+  const { get } = useApi("posts");
+
+  useEffect(() => {
+    const fetchFeed = async () => {
+      const response = await get("/feed");
+      setFeed(response.data);
+    };
+    fetchFeed();
+  }, []);
+
   return (
     <div>
       <Header />
       <div className={styles.page}>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        {feed.map((feed) => (
+          <Post />
+        ))}
       </div>
       <Navbar />
     </div>
