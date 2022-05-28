1c13c78ad423d96fa1895cfee53e901e1c727ebf
Aléxia Dorneles <alexiadorneles02@gmail.com>
2022-05-28 08:01:08 -0300

    feat(info): add page

--- frontend/src/pages/info/Info.tsx
+++ frontend/src/pages/info/Info.tsx
@@ -9,7 +9,7 @@ export function Info() {
     <>
       <Header />
       <div className={styles.page}>
-        <div className="flex items-center justify-evenly pt-5 text-lg">
+        <div className="flex items-center justify-evenly pt-8 text-lg">
           <div className="flex flex-col">
             <span className={styles.title}>87</span>
             <p>Casos</p>
@@ -21,6 +21,33 @@ export function Info() {
               <span>Denuncie</span>
             </span>
           </div>
+        </div>
+
+        <div className={styles.container}>Leis Importantes</div>
+        <div className="mt-2 p-5">
+          <span className={styles.primary}>
+            <span className="font-bold">
+              Lei Maria da Penha - Lei n. 14.310
+            </span>{" "}
+            de 08 de março de 2022
+          </span>{" "}
+          alterou a Lei Maria da Penha para determinar o registro imediato, pela
+          autoridade judicial, das medidas protetivas de urgência deferidas em
+          favor da mulher em situação de violência doméstica e familiar, ou de
+          seus dependentes.
+        </div>
+        <div className="mt-1 p-5 pt-2">
+          <span className={styles.primary}>
+            <span className="font-bold"> Lei Carolina Dieckmann</span>
+            (Lei nº 12.737/2012).
+          </span>{" "}
+          definiu crimes cibernéticos no Brasil. Ela recebeu este nome, pois na
+          época que o projeto tramitava a atriz teve o computador invadido e
+          fotos pessoais divulgadas sem autorização por hackers. A legislação
+          classifica como crime justamente casos como estes: invasão de
+          computadores, tablets, smartphones, conectados ou não à internet, que
+          resulte na obtenção, adulteração ou destruição dos dados e
+          informações.{" "}
         </div>
       </div>
       <Navbar />
