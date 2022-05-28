bac29a48f72f0fb4942bf72e4fd152275965d775
Aléxia Dorneles <alexiadorneles02@gmail.com>
2022-05-28 06:13:40 -0300

    feat(feed): blur when warning

--- frontend/src/components/post/Post.tsx
+++ frontend/src/components/post/Post.tsx
@@ -1,12 +1,14 @@
 import clsx from "clsx";
 import { FeedPost } from "../../types/post";
 import styles from "./Post.module.css";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 
 interface PostProps {
   post: FeedPost;
 }
 
 export function Post({ post }: PostProps) {
+  const hasWarning = Boolean(post.triggerWarning);
   return (
     <div className="flex flex-col p-6">
       <div className="flex items-center gap-2">
@@ -22,9 +24,23 @@ export function Post({ post }: PostProps) {
         </div>
         <p className={styles.title}>{post.user.name}</p>
       </div>
-      <div className="mt-1">
-        <p>{post.content}</p>
+      <div className={clsx("mt-1 relative")}>
+        <p className={clsx(hasWarning && styles.hide)}>{post.content}</p>
+        {hasWarning && (
+          <p className={clsx("absolute w-fit drop-shadow-sm", styles.view)}>
+            Visualizar mesmo assim
+          </p>
+        )}
       </div>
+      {hasWarning && (
+        <span className="flex items-center gap-3 mt-3">
+          <WarningAmberIcon color="primary" />
+          <p className={styles.warning}>
+            {"Alerta de gatilho: "}
+            {post.triggerWarning}
+          </p>
+        </span>
+      )}
     </div>
   );
 }
