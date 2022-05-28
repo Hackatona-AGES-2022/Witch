ca235d716a0490f030bd794aaf67d87740e978f1
Aléxia Dorneles <alexiadorneles02@gmail.com>
2022-05-28 04:14:41 -0300

    refactor(navbar): rename folder

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "../../components/header/Navbar";
+import { Navbar } from "../../components/navbar/Navbar";
 import styles from "./Home.module.css";
 
 export function Home() {
